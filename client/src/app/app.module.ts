586b687a48569fe5d636be38ef7c0ea6b2e36b53
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-03-13 17:57:19 +0700

    Page layout setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatList, MatListModule } from '@angular/material/list';
 
 const components = [
-  MatToolbarModule
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule
 ]
 
 @NgModule({
